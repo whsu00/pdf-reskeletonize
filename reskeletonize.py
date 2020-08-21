969db99b41e51f2ed23b4d06b3cca93d8f8b6a4c
William Hsu <whsu00@berkeley.edu>
2020-08-20 21:52:08 -0700

    Add page size scaling

--- reskeletonize.py
+++ reskeletonize.py
@@ -3,6 +3,7 @@ import json
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
 UNITS_PER_INCH = 72
+PAGE_SIZE = (8.5, 11)
 
 @click.command()
 @click.option('--outline-pdf', type=click.File(mode='rb'), default="outline.pdf", prompt=True, help="PDF file of the outline")
@@ -18,6 +19,8 @@ def main(outline_pdf, outline_json, submission_pdf, submission_json, output_pdf)
     submission = PdfFileReader(submission_pdf)
     outline_pages = [outline.getPage(i) for i in range(outline.getNumPages())]
     submission_pages = [submission.getPage(i) for i in range(submission.getNumPages())]
+    for page in submission_pages:
+        page.scaleTo(PAGE_SIZE[0] * UNITS_PER_INCH, PAGE_SIZE[1] * UNITS_PER_INCH)
     for outline_obj in outline_data:
         for submission_obj in submission_data:
             if outline_obj["id"] == submission_obj["id"]:
