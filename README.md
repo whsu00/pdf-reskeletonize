f95104965099d76008efc1364baa59e2b787a095
William Hsu <whsu00@berkeley.edu>
2020-08-20 18:33:57 -0700

    Add proof-of-concept script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# pdf-reskeletonize
-Proof of concept for re-skeletonizing PDFs
+# PDF Reskeletonize
+Proof of concept for re-skeletonizing PDFs.
+
+## Requirements
++ PyPDF2 with (PR #240)[https://github.com/mstamy2/PyPDF2/pull/240] applied
+
+## Usage
+`python reskeletonize.py outline.pdf outline.json submission.pdf submission.json reskeletonized.pdf`
+
+`outline.json` specifies a list of template objects in the outline PDF, their x/y coordinates, and their width & height.
+
+`submission.json` specifies a list of objects from the template that are present in the submission PDF, and their x/y coordinates.
+
+Coordinates are all in inches, and the origin is at the bottom left of a page. PDF pages are 1-indexed (page 1 is the first page of a PDF).
---
+++ reskeletonize.py
@@ -0,0 +1,35 @@
+import click
+import json
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+UNITS_PER_INCH = 72
+
+@click.command()
+@click.option('--outline-pdf', type=click.File(mode='rb'), default="outline.pdf", prompt=True, help="PDF file of the outline")
+@click.option('--outline-json', type=click.File(), default="outline.json", prompt=True, help="JSON file describing components of the outline")
+@click.option('--submission-pdf', type=click.File(mode='rb'), default="submission.pdf", prompt=True, help="PDF file of the submission")
+@click.option('--submission-json', type=click.File(), default="submission.json", prompt=True, help="JSON file describing components of the submission")
+@click.option('--output-pdf', type=click.File(mode='wb'), default="output.pdf", prompt=True, help="Output path of the reskeletonized PDF file")
+def main(outline_pdf, outline_json, submission_pdf, submission_json, output_pdf):
+    """Proof of concept for reskeletonizing PDFs."""
+    outline_data = json.load(outline_json)
+    submission_data = json.load(submission_json)
+    outline = PdfFileReader(outline_pdf)
+    submission = PdfFileReader(submission_pdf)
+    outline_pages = [outline.getPage(i) for i in range(outline.getNumPages())]
+    submission_pages = [submission.getPage(i) for i in range(submission.getNumPages())]
+    for outline_obj in outline_data:
+        for submission_obj in submission_data:
+            if outline_obj["id"] == submission_obj["id"]:
+                w, h = outline_obj["w"], outline_obj["h"]
+                page = submission_pages[submission_obj["page"] - 1]
+                page.trimBox.lowerLeft = (submission_obj["x"] * UNITS_PER_INCH, submission_obj["y"] * UNITS_PER_INCH)
+                page.trimBox.upperRight = ((submission_obj["x"] + w) * UNITS_PER_INCH, (submission_obj["y"] + h) * UNITS_PER_INCH)
+                outline_pages[outline_obj["page"] - 1].mergeTranslatedPage(page, (outline_obj["x"] - submission_obj["x"]) * UNITS_PER_INCH, (outline_obj["y"] - submission_obj["y"]) * UNITS_PER_INCH)
+    output = PdfFileWriter()
+    for page in outline_pages:
+        output.addPage(page)
+    output.write(output_pdf)
+
+if __name__ == '__main__':
+    main()
